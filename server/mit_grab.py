742f67faf84dd60459ef589c1a19272bdfc0582d
DarKnight <amit.gupta.ece13@iitbhu.ac.in>
2015-03-22 01:55:25 +0530

    Added max_limit in mit_course function to limit return search result
    # raises error if max_limit is set to larger value than 10
    # minor changes in doc string

--- server/mit_grab.py
+++ server/mit_grab.py
@@ -7,12 +7,17 @@ import urllib2
 from bs4 import BeautifulSoup
 
 
-def mit_course(query):
+def mit_course(query, max_limit=2):
     """
     Given function returns the courses related to the query provided
     :param query: course name to be searched on the mit.edu
+    :param max_limit: sets maximum search result to return (maximum value is 10)
     :return: returns the resulted search json file
+    :raises: raise the error if max_limit is set larger than 10
     """
+    if max_limit > 10:
+        raise Exception("max_limit must be less than 10")
+
     a = query.split()
     query = "http://search.mit.edu/search?site=ocw&client=mit&getfields=*&" +\
                 "output=xml_no_dtd&proxystylesheet=http%3A%2F%2Focw.mit.edu%2Fsearch%2Fgoogle-ocw" +\
@@ -32,10 +37,12 @@ def mit_course(query):
         data += str(i)
     soup = BeautifulSoup(data)
     data = '{"mit_ocw":['
+    j = 1
     for i in soup.find_all('a'):
         data += '{"' + i.get_text() + '":"' + i.get('href') + '"},'
+        if j == max_limit:
+            break
+        j += 1
     data += "]}"
     data = data.encode('ascii', errors='ignore')
     return data
-
-print mit_course("Machine learning")
