7e8806f217ef02e769b09438309fd1eece057370
Dushyant Mishra <zero1hac@users.noreply.github.com>
2015-03-22 12:34:37 +0530

    Database creation automatically

--- server/searcher.py
+++ server/searcher.py
@@ -6,10 +6,15 @@ def search(query):
     rating = 0.0
     query = query.replace('+', ' ')
     result = ''
-    connection = MySQLdb.connect("localhost", "root", "newpass@123", "code42day")
+    connection = MySQLdb.connect("localhost", "root", "newpass@123","code42day")#create database manually
+    '''change hostname from localhost to your server IP
+    change user from 'root' to your user
+    change pass to your user's pass
+    '''
     query1 = query
     query = query.replace(' ', '').lower()
     cur = connection.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS Searches (striptext VARACHAR(50),search TEXT,rating FLOAT")
     d = cur.execute("SELECT * FROM Searches WHERE striptext='%s';" % (str(query))  )
     if (d):
         result = cur.fetchall()
